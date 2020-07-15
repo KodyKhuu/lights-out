d66caf8e2d428416463374e1bb99a8a6fa8a34c0
Kody Khuu <khuukody82122@gmail.com>
2020-07-14 22:44:41 -0700

    Updates

---
+++ precache-manifest.496eadc77e5d223de0988a5c2fb5e405.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "60f5575d83b8af98ab3d",
+    "url": "/lights-out/static/js/main.60f5575d.chunk.js"
+  },
+  {
+    "revision": "9ee2bac8e8e8a44de14d",
+    "url": "/lights-out/static/js/1.9ee2bac8.chunk.js"
+  },
+  {
+    "revision": "60f5575d83b8af98ab3d",
+    "url": "/lights-out/static/css/main.9f0d9439.chunk.css"
+  },
+  {
+    "revision": "5d24e7d1dc4db43354eeaef9ab9787e4",
+    "url": "/lights-out/index.html"
+  }
+];
